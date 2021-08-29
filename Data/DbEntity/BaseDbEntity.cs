fbd208a0741ab8a088c40c1dbc36ef300b8eca43
OsamaRaed <63412382+OsamaRaed@users.noreply.github.com>
2021-08-29 13:35:17 +0300

    base Entity added

---
+++ Data/DbEntity/BaseDbEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Majles.Data.DbEntity
+{
+    public class BaseDbEntity
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public BaseDbEntity()
+        {
+            CreatedAt = DateTime.Now;
+        }
+    }
+}
