6d283ae06971961d83b3207f22a48f7f09478a85
OsamaRaed <63412382+OsamaRaed@users.noreply.github.com>
2021-08-30 19:42:33 +0300

    dbcontext

---
+++ Data/Data/MajlesDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Majles.Data.Data
+{
+    public class MajlesDbContext
+    //public class MajlesDbContext : DbContext
+    {
+    }
+}
