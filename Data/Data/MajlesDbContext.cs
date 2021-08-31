ae064f2a9474f7d7a5dac3c37076f323371624df
OsamaRaed <63412382+OsamaRaed@users.noreply.github.com>
2021-08-31 16:31:43 +0300

    dbentities added to context as Dbsets

--- Data/Data/MajlesDbContext.cs
+++ Data/Data/MajlesDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Majles.Data.DbEntity;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,27 @@ using System.Threading.Tasks;
 
 namespace Majles.Data.Data
 {
-    public class MajlesDbContext
-    //public class MajlesDbContext : DbContext
+    public class MajlesDbContext : DbContext
     {
+        public DbSet<AttendenceDbEntity> Attendances { get; set; }
+        public DbSet<AuthorBookDbEntity> AuthorBooks { get; set; }
+        public DbSet<AuthorDbEntity> Authors { get; set; }
+        public DbSet<BookCategoryDbEntity> BookCategories { get; set; }
+        public DbSet<BookDbEntity> Books { get; set; }
+        public DbSet<BookMajlesDbEntity> BookMajles { get; set; }
+        public DbSet<CategoryDbEntity> Categories { get; set; }
+        public DbSet<LectureDbEntity> Lectures { get; set; }
+        public DbSet<MajlesDbEntity> Majles { get; set; }
+        public DbSet<MajlesStudentDbEntity> MajlesStudents { get; set; }
+        public DbSet<MajlesVolunteerDbEntity> MajlesVolunteers { get; set; }
+        public DbSet<MosqeDbEntity> Mosqes { get; set; }
+        public DbSet<RegionDbEntity> Regions { get; set; }
+        public DbSet<SheikhDbEntity> Sheikhs { get; set; }
+        public DbSet<SheikhPhoneDbEntity> SheikhPhones { get; set; }
+        public DbSet<StudentDbEntity> Students { get; set; }
+        public DbSet<StudentPhoneDbEntity> StudentPhones { get; set; }
+        public DbSet<VolunteerDbEntity> Volunteers { get; set; }
+        public DbSet<VolunteerPhoneDbEntity> VolunteerPhones { get; set; }
+
     }
 }
