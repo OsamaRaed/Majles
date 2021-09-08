b76b95b1a83f89bf0d0bacfa442b55d7d52ec229
OsamaRaed <63412382+OsamaRaed@users.noreply.github.com>
2021-09-08 15:24:49 +0300

    primary key bug fixed in mosqe and phone [key] added

--- Data/DbEntity/PhoneDbEntity.cs
+++ Data/DbEntity/PhoneDbEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Majles.Data.DbEntity
 {
     public class PhoneDbEntity : BaseDbEntity
     {
+        [Key]
         public string PhoneNumber { get; set; }
     }
 }
