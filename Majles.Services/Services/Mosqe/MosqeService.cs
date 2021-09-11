4ecffed57ff751ec8a790be21de0c3d0b6aa42e6
OsamaRaed <63412382+OsamaRaed@users.noreply.github.com>
2021-09-11 19:16:03 +0300

    services added

---
+++ Majles.Services/Services/Mosqe/MosqeService.cs
@@ -0,0 +1,32 @@
+﻿using Majles.Data.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Majles.Services.Services.Mosqe
+{
+    public class MosqeService : IMosqeService
+    {
+        private readonly MajlesDbContext dbContext;
+
+        public MosqeService(MajlesDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void Create(string name)
+        {
+            dbContext.Mosqes.FromSqlInterpolated
+                ($"insert into Mosqes (Name,CreatedAt,UpdatedAt) values('{name}','{DateTime.Now}');");
+        }
+
+        public void Delete(string name)
+        {
+            dbContext.Mosqes.FromSqlInterpolated
+                ($"delete from Mosqes where Name = '{name}'");
+        }
+    }
+}
