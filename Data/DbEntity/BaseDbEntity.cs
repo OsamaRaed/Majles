f8fb2eb175fbd34e46c4d410bfb2fc0ffc75ccd0
OsamaRaed <63412382+OsamaRaed@users.noreply.github.com>
2021-09-11 19:16:23 +0300

    update base entity

--- Data/DbEntity/BaseDbEntity.cs
+++ Data/DbEntity/BaseDbEntity.cs
@@ -9,7 +9,7 @@ namespace Majles.Data.DbEntity
     public class BaseDbEntity
     {
         public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
         public BaseDbEntity()
         {
             CreatedAt = DateTime.Now;
